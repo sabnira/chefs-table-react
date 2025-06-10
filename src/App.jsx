deca2fb26e45ecd6f35191c1f4fc96296e4c9b31
sabnira <sabnirajeemi@gmail.com>
2025-06-10 16:34:15 +0600

    card functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Banner from './components/Banner';
 import OurRecipes from './components/OurRecipes';
@@ -6,6 +6,23 @@ import Recipes from './components/Recipes';
 import Sidebar from './components/Sidebar';
 
 const App = () => {
+
+  const [recipeQueue, setRecipeQueue] = useState([])
+
+  const addRecipeToQueue = recipe => {
+
+    const isExist = recipeQueue.find(
+      previousRecipe => previousRecipe.recipe_id === recipe.recipe_id
+    )
+
+    if(!isExist){
+      setRecipeQueue([...recipeQueue, recipe])
+    } else {
+      alert('Recipe already exists in the queue.')
+    }
+  }
+  console.log(recipeQueue);
+
   return (
     <div className='w-10/12 mx-auto'>
       <Header></Header>
@@ -13,7 +30,8 @@ const App = () => {
       <OurRecipes></OurRecipes>
 
       <section className='flex flex-col md:flex-row gap-6'>
-        <Recipes></Recipes> 
+        <Recipes addRecipeToQueue=
+        {addRecipeToQueue}></Recipes> 
         <Sidebar></Sidebar>
       </section>
 
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
-const Recipes = () => {
+const Recipes = ({addRecipeToQueue}) => {
 
     const [recipes, setRecipes] = useState([]);
 
@@ -10,7 +10,7 @@ const Recipes = () => {
         .then(data => setRecipes(data))
     }, [])
 
-    console.log(recipes);
+
     return (
         <div className='md:w-2/3'>
 
@@ -18,29 +18,45 @@ const Recipes = () => {
 
             {recipes.map(recipe => (
 
-            <div key={recipe.recipe_id} className="card bg-base-100 shadow-sm">
+            <div key={recipe.recipe_id} className="card bg-base-100 border-1">
                 <figure className='px-8 pt-6'>
                     <img className='w-full md:h-52 object-cover rounded-xl'
                     src={recipe.recipe_image}
                     alt="recipes image" />
                 </figure>
                 <div className="card-body">
-                    <h2 className="card-title text-lg text-gray-800 font-medium">
+                    <h2 className="text-xl text-gray-800 font-semibold">
                         {recipe.recipe_name}
                     </h2>
-                    <p className="text-gray-600">
-                        {recipe.short_description}
-                    </p>
+
+                    <h2 className="text-gray-600">{recipe.short_description}</h2>
 
                     <h3 className='text-lg text-gray-800 font-medium'>Ingredients: {recipe.ingredients.length}</h3>
+
                     <ul className='ml-8'>
                         {recipe.ingredients.map((item, index) => (
                             <li className='list-disc text-gray-600' key={index}>{item}</li>
                         ))}
                     </ul>
 
-                    <div className="card-actions justify-end">
-                    <button className="btn btn-success">Buy Now</button>
+                    <p></p>
+                    
+                    <div className='flex gap-4 mt-4'>
+                        <div className='flex items-center'>
+                            <i className="fa-regular fa-clock mr-2 text-2xl"></i>
+                            <p>{recipe.preparing_time} minute.</p>
+                        </div>
+
+                        <div className='flex items-center'>
+                            <i className="fa-solid fa-fire-flame-curved mr-2 text-2xl"></i>
+                            <p>{recipe.calories} calorie.</p>
+                        </div>
+                    </div>
+
+                   
+
+                    <div onClick={()=>addRecipeToQueue(recipe)} className="card-actions ">
+                    <button className="btn btn-success rounded-full px-8  text-gray-800 mt-6 font-medium">Want To Cook</button>
                     </div>
                 </div>
             </div>
